019f0da204e96dbb6c60632d09bc92401d7bb126
R. Tyler Croy <tyler@monkeypox.org>
2015-08-08 14:24:19 -0700

    Start adding some code, basics to make codenarc/groovy/spock work together

---
+++ src/main/groovy/com/github/jrubygradle/groovygem/Gem.groovy
@@ -0,0 +1,9 @@
+package com.github.jrubygradle.groovygem
+
+/**
+ * Plain Old Groovy Object wrapping the data for a Gem
+ */
+class Gem {
+    String name
+    String version
+}
---
+++ src/test/groovy/com/github/jrubygradle/groovygem/GemSpec.groovy
@@ -0,0 +1,6 @@
+package com.github.jrubygradle.groovygem
+
+import spock.lang.*
+
+class GemSpec extends Specification {
+}
