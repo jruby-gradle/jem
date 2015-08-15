f1641a3a1861b24688c2ec352f8404a702c7570b
Christian Meier <meier.kristian@gmail.com>
2015-08-15 20:23:03 +0200

    added a JemClassLoader which can "add" jem on the fly

--- src/main/java/com/github/jrubygradle/jem/Gem.java
+++ src/main/java/com/github/jrubygradle/jem/Gem.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
